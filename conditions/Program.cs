ff751c98606b8d9eca17fe5fa521af7794b941a3
Egbujie Chizoba <egbujie.chizoba@gmail.com>
2022-11-09 16:35:27 +0100

    Add files via upload

---
+++ conditions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+ namespace Conditions
+{
+    class Conditions
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+
+
+        }
+        }
+        }
