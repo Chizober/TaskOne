2d05578e3cb849b4abf8538f718dd13fb89d0d42
Egbujie Chizoba <egbujie.chizoba@gmail.com>
2022-11-09 16:34:08 +0100

    Add files via upload

---
+++ librarySystem/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+ namespace exercises
+{
+    class exercises
+    {
+
+
+        static void Main(string[] args)
+        {
+            string user = "";
+            Console.WriteLine("Please pick a book");
+            user = Console.ReadLine();
+            switch (user)
+            {
+                case "c":
+                case "C":
+                    Console.WriteLine("C# books");
+                    break;
+                case "o":
+                case "O":
+                    Console.WriteLine(".NET core books");
+                    break;
+                case "d":
+                case "D":
+                    Console.WriteLine("clean coding books");
+                    break;
+                case "e":
+                case "E":
+                    Console.WriteLine(".EFCore");
+                    break;
+                default:
+                    Console.WriteLine("try again");
+                    break;
+            }
+
+
+        }
+}
+}
+
+
+
