9769afda6b475663bd77a3731386a469aa12d8b8
Egbujie Chizoba <egbujie.chizoba@gmail.com>
2022-11-09 16:52:54 +0100

    Add files via upload

---
+++ date/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+  Console.WriteLine("Enter Date in format dd/MMM/yy");
+    string userDate = Console.ReadLine();
+    dateFunction(userDate);
+    static void dateFunction(string userDate)
+    {
+      DateTime UserDate = DateTime.Parse(userDate);
+      DateTime solution =UserDate.AddDays(7);
+      Console.WriteLine($"{solution.ToString("dd/MMM/yy")}");
+    }
+
