c443dca35c8638b67cb03c3340f840380461fcd4
Egbujie Chizoba <egbujie.chizoba@gmail.com>
2022-11-10 14:46:11 +0100

    Add files via upload

---
+++ capitalIndices/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter string with capital letter");
+string  userInput = Console.ReadLine();
+List<int> indices = new List<int>();
+for(int i =0; i<= userInput.Length-1; i++)
+{
+  if(userInput[i] >='A' && userInput[i] <='Z')
+  {
+    indices.Add(i);
+  }
+}
+foreach(int index in indices)
+{
+  Console.WriteLine($"Index:{index}\t Value:{userInput[index]}");
+}
